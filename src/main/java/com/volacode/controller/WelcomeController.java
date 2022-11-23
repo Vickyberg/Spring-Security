5370903940d70bd2e22c2fb11c2cb060a063a1af
Victor Olamide <akinnusivictor098@gmail.com>
2022-11-23 23:57:23 +0000

    starting up SS

---
+++ src/main/java/com/volacode/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.volacode.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public  String sayWelcome(){
+        return "Welcome to spring Application with Security";
+    }
+}
