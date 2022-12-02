870197b2239f4d7c6ec29ee9cbe1294c1ff1e8dd
Victor Olamide <akinnusivictor098@gmail.com>
2022-12-02 08:51:22 +0000

    Bank App With Authorisation

--- src/main/java/com/volacode/config/ProjectSecurityConfig.java
+++ src/main/java/com/volacode/config/ProjectSecurityConfig.java
@@ -3,6 +3,11 @@ package com.volacode.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,13 +25,36 @@ public class ProjectSecurityConfig {
 
     }
 
-    /*@Bean
-    SecurityFilterChain defaultSecurityToDenyAll(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().anyRequest().denyAll().and().httpBasic().and().formLogin();
-        return httpSecurity.build();
-    }
-    
-     */
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+    /*    UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin,user);
 
+     */
+        UserDetails admin = User.withUsername("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withUsername("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin,user);
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+       return NoOpPasswordEncoder.getInstance();
+    }
 
 }
